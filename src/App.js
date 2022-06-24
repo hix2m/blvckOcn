f0159d810b347c44aea6167b803bf7f608390464
hix2m <allouni.hicham@ofppt-edu.ma>
2022-06-24 23:17:26 +0100

    some updates

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
       <div className={classes.App}>
         <Header />
         <Route path="/blvckOcn/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact />
+        <Route path="/blvckOcn/coins/:id" component={CoinPage} exact />
       </div>
     </BrowserRouter>
   );
